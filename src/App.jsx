b405bc11dcc27069c8ce1b613520e1b889d6d0d0
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2026-08-31 21:05:09 -0300

    atualização para git pages

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function Principal() {
 
 function App() {
     return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Principal />} />
 
